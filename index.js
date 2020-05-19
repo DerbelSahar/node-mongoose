0f82e78c1b3b82b79e0953e2092837277edf8e2c
Sahar Derbel <derbel.sahar@insat.u-carthage.tn>
2020-05-19 13:27:50 +0100

    mangoose part1

--- index.js
+++ index.js
@@ -10,10 +10,25 @@ connect.then((db) => {
             })
             .then((dish) => {
                 console.log(dish);
-                return Dishes.find({}).exec();
+                return Dishes.findByIdAndUpdate(dish._id,
+                    { 
+                        $set: {description: 'updated test'}
+
+                    },{
+                        new: true
+                    }
+                    ).exec();
             })
-            .then((dishes) => {
-                console.log(dishes);
+            .then((dish) => {
+                console.log(dish);
+                dish.comments.push({
+                    rating: 5,
+                    comment: 'good dish',
+                    author: 'leonardo',
+                });
+                return dish.save(); //returns the dish
+            })
+            .then(() => {
                 return Dishes.remove({});
  
             })
--- models/dishes.js
+++ models/dishes.js
@@ -1,5 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment: {
+        type: String,
+        require: true
+
+    },
+    author: {
+        type: String,
+        required: true
+    }}, {
+        timestamps: true
+    
+});
 const dishSchema = new Schema({
 
     name: {
@@ -10,7 +29,9 @@ const dishSchema = new Schema({
     description: {
         type: String,
         required: true
-    }}, {
+    },
+    comments: [ commentSchema ] //array of comments
+    }, {
         timestamps: true
     }
 );
